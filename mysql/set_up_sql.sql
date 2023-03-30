fb88487462aca7238dc92510356a61d290a5e760
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2023-03-30 14:32:01 +0200

    Create set_up_sql.sql

---
+++ mysql/set_up_sql.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE chatbotApp;
+
+CREATE TABLE Users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL
+);
