e0af5be86282815396ef837516685796b0adfa5a
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2023-04-11 15:24:50 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -26,7 +26,7 @@ app.get("/", (req: express.Request, res: express.Response) => {
     });
 });
 
-app.use("/login", loginRouter);
+app.use("/auth", loginRouter);
 app.use("/auth", registerRouter);
 app.use("/", userDataRouter);
 
